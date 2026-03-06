3c45867cc42ee0f3545dd9a18924371440d49801
maxim-pacini <maxim10pacini@gmail.com>
2026-03-06 14:43:59 +0100

    starting

---
+++ game.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="box">
+        <img src="./images/coins.png" alt="">
+        
+        <?php echo date('d/m/Y'); ?>
+    </div>
+    
+</body>
+</html>
